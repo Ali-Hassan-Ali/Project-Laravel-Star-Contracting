55b0f5850d77488b395832d81c5806316c61c8d3
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-05-24 12:44:46 +0200

    add name admin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,8 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('البريد الكتروني ') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                {{-- <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> --}}
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="super_admin@app.com" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +30,8 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('كلمة السر ') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                {{-- <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password"> --}}
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" value="123123123" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
