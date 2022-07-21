c9938e8e41c20a3da9d2f09b72f83d71b2b70f19
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-07-21 14:25:01 +0200

    add multe combox

--- resources/views/admin/combo_boxs/edit.blade.php
+++ resources/views/admin/combo_boxs/edit.blade.php
@@ -29,7 +29,7 @@
                     </div>
 
                     @php
-                        $combo_boxs = ['make', 'model', 'owner_ship'];
+                        $combo_boxs = ['make', 'model', 'owner_ship', 'equipment', 'equipment', 'rental_basis', 'operator', 'responsible_person', 'responsible_person_email', 'allocated_to', 'project_allocated_to'];
                     @endphp
 
                     {{--type--}}
