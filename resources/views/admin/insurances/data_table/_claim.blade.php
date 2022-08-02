0649780fb7aa8de14fe48ffea2f5a30a24c9ee68
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-08-02 09:01:40 +0200

    add

--- resources/views/admin/insurances/data_table/_claim.blade.php
+++ resources/views/admin/insurances/data_table/_claim.blade.php
@@ -2,6 +2,5 @@
 <div class="form-group ml-3">
     <div class="form-check form-switch">
       <input class="form-check-input" id="claim" type="checkbox" name="claim" data-id="{{ $insurance->id }}" value="{{ old('claim', $insurance->claim) }}" {{ $insurance->claim == '1' ? 'checked' : '' }} disabled>
-      <label class="form-check-label">@lang('insurances.claim')</label>
     </div>
 </div>
