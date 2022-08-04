3808c0cbbc0a32a00b68a80431b412fa9d65e625
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-08-04 10:09:14 +0200

    add

--- resources/views/admin/equipments/data_table/_project_allocated_to.blade.php
+++ resources/views/admin/equipments/data_table/_project_allocated_to.blade.php
@@ -1,4 +1,7 @@
-
-@foreach (json_decode($equipment->project_allocated_to) as $data)
-	<span class="badge badge-primary">{{ $data }}</span>
-@endforeach
+@if ($equipment->project_allocated_to)
+	
+	@foreach (json_decode($equipment->project_allocated_to) as $data)
+		<span class="badge badge-primary">{{ $data }}</span>
+	@endforeach
+	
+@endif
