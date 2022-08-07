6bc04d3c86abe5926445b31e7271cee6c445e61c
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-08-07 14:59:32 +0200

    add

--- resources/views/admin/spares/edit.blade.php
+++ resources/views/admin/spares/edit.blade.php
@@ -24,10 +24,10 @@
 
                     @include('admin.partials._errors')
 
-                    <div class="row mb-1">
+                    <div class="row">
                         
                         {{--equipment_id--}}
-                        <div class="col-6">
+                        <div class="form-group col-6">
                             <label>@lang('countrys.countrys') <span class="text-danger">*</span></label>
                             <select class="form-control col-6 select2" id="equipment-countrey">
                                 <option value="" selected disabled>@lang('site.choose') @lang('countrys.countrys')</option>
@@ -39,83 +39,86 @@
                         </div>
 
                         {{--equipment_id--}}
-                        <div class="col-6">
+                        <div class="form-group col-6">
                             <label>@lang('citys.citys') <span class="text-danger">*</span></label>
                             <select class="form-control select2" id="equipment-city">
                                 
                             </select>
                         </div>
 
+                        {{-- equipment --}}
+                        <div class="form-group @error('equipment_id') custom-select @enderror">
+                            <label>@lang('equipments.equipments') <span class="text-danger">*</span></label>
+                            <select name="equipment_id" id="equipment-man" class="form-control select2" required>
+                                <option value="">@lang('site.choose') @lang('equipments.equipments')</option>
+                                @foreach ($equipments as $equipment)
+                                    <option value="{{ $equipment->id }}" {{ $equipment->id == old('equipment_id', $spare->equipment_id) ? 'selected' : '' }}>{{ $equipment->name .' '. $equipment->make .' '. $equipment->plate_no }}</option>
+                                @endforeach
+                            </select>
+                            @error('equipment_id')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        {{--name--}}
+                        <div class="form-group col-6">
+                            <label>@lang('spares.name')<span class="text-danger">*</span></label>
+                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $spare->name) }}" required autofocus>
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        {{--part_no--}}
+                        <div class="form-group col-6">
+                            <label>@lang('spares.part_no')<span class="text-danger">*</span></label>
+                            <input type="text" name="part_no" class="form-control @error('part_no') is-invalid @enderror" value="{{ old('part_no', $spare->part_no) }}" required autofocus>
+                            @error('part_no')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+
+                        {{--cost--}}
+                        <div class="form-group col-6">
+                            <label>@lang('spares.cost') <span class="text-danger">*</span></label>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">$</span>
+                                </div>
+                                <input type="number" name="cost" class="form-control @error('cost') is-invalid @enderror" value="{{ old('cost', $spare->cost) }}" required>
+                                @error('cost')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        
+
+                        {{--freight_charges--}}
+                        <div class="form-group col-6">
+                            <label>@lang('spares.freight_charges') <span class="text-danger">*</span></label>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">$</span>
+                                </div>
+                                <input type="number" name="freight_charges" class="form-control @error('freight_charges') is-invalid @enderror" value="{{ old('freight_charges', $spare->freight_charges) }}" required>
+                                @error('freight_charges')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
                     </div>{{-- row --}}
-
-                    {{-- equipment --}}
-                    <div class="form-group @error('equipment_id') custom-select @enderror">
-                        <label>@lang('equipments.equipments') <span class="text-danger">*</span></label>
-                        <select name="equipment_id" id="equipment-man" class="form-control select2" required>
-                            <option value="">@lang('site.choose') @lang('equipments.equipments')</option>
-                            @foreach ($equipments as $equipment)
-                                <option value="{{ $equipment->id }}" {{ $equipment->id == old('equipment_id', $spare->equipment_id) ? 'selected' : '' }}>{{ $equipment->name .' '. $equipment->make .' '. $equipment->plate_no }}</option>
-                            @endforeach
-                        </select>
-                        @error('equipment_id')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-
-                    {{--name--}}
-                    <div class="form-group">
-                        <label>@lang('spares.name')<span class="text-danger">*</span></label>
-                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $spare->name) }}" required autofocus>
-                        @error('name')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-
-                    {{--part_no--}}
-                    <div class="form-group">
-                        <label>@lang('spares.part_no')<span class="text-danger">*</span></label>
-                        <input type="text" name="part_no" class="form-control @error('part_no') is-invalid @enderror" value="{{ old('part_no', $spare->part_no) }}" required autofocus>
-                        @error('part_no')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-
-
-                    {{--cost--}}
-                    <label>@lang('spares.cost') <span class="text-danger">*</span></label>
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">$</span>
-                        </div>
-                        <input type="number" name="cost" class="form-control @error('cost') is-invalid @enderror" value="{{ old('cost', $spare->cost) }}" required>
-                        @error('cost')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    
-
-                    {{--freight_charges--}}
-                    <label>@lang('spares.freight_charges') <span class="text-danger">*</span></label>
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">$</span>
-                        </div>
-                        <input type="number" name="freight_charges" class="form-control @error('freight_charges') is-invalid @enderror" value="{{ old('freight_charges', $spare->freight_charges) }}" required>
-                        @error('freight_charges')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-
 
                     {{--$used--}}
                     <div class="form-group ml-3">
