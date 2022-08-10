f88e18997e1555b2f4d71e7a1c6cee48ea5a23db
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-08-10 15:21:25 +0200

    add

--- resources/views/admin/eirs/index.blade.php
+++ resources/views/admin/eirs/index.blade.php
@@ -73,12 +73,12 @@
                                     {{-- <th>@lang('eirs.expected_payment_transfer_date')</th> --}}
                                     {{-- <th>@lang('eirs.expected_shipment_pickup_date')</th> --}}
                                     {{-- <th>@lang('eirs.expected_arrival_to_site_date')</th> --}}
+                                    <th>@lang('eirs.status')</th>
                                     <th>@lang('eirs.actual_process_date')</th>
                                     <th>@lang('eirs.actual_po_released_date')</th>
                                     <th>@lang('eirs.actual_payment_transfer_date')</th>
                                     <th>@lang('eirs.actual_shipment_pickup_date')</th>
                                     <th>@lang('eirs.actual_arrival_to_site_date')</th>
-                                    <th>@lang('eirs.status')</th>
                                     <th>@lang('eirs.attachments')</th>
                                     <th>@lang('site.action')</th>
                                 </tr>
@@ -123,12 +123,12 @@
                 // {data: 'expected_payment_transfer_date', name: 'expected_payment_transfer_date'},
                 // {data: 'expected_shipment_pickup_date', name: 'expected_shipment_pickup_date'},
                 // {data: 'expected_arrival_to_site_date', name: 'expected_arrival_to_site_date'},
+                {data: 'status', name: 'status'},
                 {data: 'actual_process_date', name: 'actual_process_date'},
                 {data: 'actual_po_released_date', name: 'actual_po_released_date'},
                 {data: 'actual_payment_transfer_date', name: 'actual_payment_transfer_date'},
                 {data: 'actual_shipment_pickup_date', name: 'actual_shipment_pickup_date'},
                 {data: 'actual_arrival_to_site_date', name: 'actual_arrival_to_site_date'},
-                {data: 'status', name: 'status'},
                 {data: 'attachments', name: 'attachments'},
                 {data: 'actions', name: 'actions', searchable: false, sortable: false, width: '20%'},
             ],
