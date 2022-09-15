c5e800855ddff6ca42fd5074616f456f5c5dc101
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-15 13:54:53 +0200

    add

--- resources/views/admin/reports/total_insurance_cost.blade.php
+++ resources/views/admin/reports/total_insurance_cost.blade.php
@@ -9,7 +9,7 @@
 	<ul class="breadcrumb mt-2">
 		<li class="breadcrumb-item"><a href="{{ route('admin.home') }}">@lang('site.home')</a></li>
 		<li class="breadcrumb-item">@lang('reports.reports')</li>
-		<li class="breadcrumb-item">@lang('reports.total_insurance_cost')</li>
+		<li class="breadcrumb-item title-download">@lang('reports.total_insurance_cost')</li>
 	</ul>
 	
 	<div class="row">
@@ -156,7 +156,7 @@
             buttons: [{
                 footer: true,
                 extend: "pdf",
-                title: `Star-Contracting`,
+                title: $('.title-download').html() + ' - ' +"{{ now()->format('d-m-Y') }}",
                 className: 'btn btn-primary',
                 text: '<i class="fa fa-file-pdf" aria-hidden="true"></i> PDF',
                 customize: function(doc) {
