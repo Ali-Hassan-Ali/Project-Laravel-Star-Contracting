b0294efc80571d71ea58b380be93bb56433fd5a4
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-18 11:30:59 +0200

    add

--- app/Http/Requests/Admin/EquipmentRequest.php
+++ app/Http/Requests/Admin/EquipmentRequest.php
@@ -57,7 +57,7 @@ class EquipmentRequest extends FormRequest
     {
         return $this->merge([
             'rental_basis' => request()->rental_basis ?? '',
-            'rental_cost_basis' => request()->rental_cost_basis ?? '',
+            'rental_cost_basis' => request()->rental_cost_basis ?? '0',
         ]);
 
     }//end of prepare for validation
