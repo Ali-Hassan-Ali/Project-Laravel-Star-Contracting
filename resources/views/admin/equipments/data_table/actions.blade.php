a314805412bf82a778da05da00682b72c034a2fa
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-18 14:20:28 +0200

    add

--- resources/views/admin/equipments/data_table/actions.blade.php
+++ resources/views/admin/equipments/data_table/actions.blade.php
@@ -1,11 +1,17 @@
 @if (auth()->user()->hasPermission('update_equipments'))
-    <a href="{{ route('admin.equipments.edit', $id) }}" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> @lang('site.edit')</a>
+    <a href="{{ route('admin.equipments.edit', $id) }}" class="btn btn-warning btn-sm">
+        <i class="fa fa-edit"></i> 
+        {{-- @lang('site.edit') --}}
+    </a>
 @endif
 
 @if (auth()->user()->hasPermission('delete_equipments'))
     <form action="{{ route('admin.equipments.destroy', $id) }}" class="my-1 my-xl-0" method="post" style="display: inline-block;">
         @csrf
         @method('delete')
-        <button type="submit" class="btn btn-danger btn-sm delete"><i class="fa fa-trash"></i> @lang('site.delete')</button>
+        <button type="submit" class="btn btn-danger btn-sm delete">
+            <i class="fa fa-trash"></i> 
+            {{-- @lang('site.delete') --}}
+        </button>
     </form>
 @endif
