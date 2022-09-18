662a09e75b507e7de417a0c9eb826bf284252f6b
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-18 15:53:37 +0200

    add

--- app/Http/Controllers/Admin/Home/AjaxController.php
+++ app/Http/Controllers/Admin/Home/AjaxController.php
@@ -56,7 +56,7 @@ class AjaxController extends Controller
         $equipmens = Equipment::withCount('statusone','city')
                                 ->having('statusone_count', '>', '0')
                                 ->get()
-                                ->sortByDesc('city.name', 'desc');
+                                ->sortByDesc('city.name');
 
         $data = view('admin.home.includes.equipments._equipment_barkdown', compact('equipmens'));
 
