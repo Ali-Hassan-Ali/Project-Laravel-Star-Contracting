1d39aed36509dfca7fb65d5155de53ee7b1b0a53
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-19 10:38:32 +0200

    add

--- resources/views/admin/eirs/data_table/_idle.blade.php
+++ resources/views/admin/eirs/data_table/_idle.blade.php
@@ -1,6 +1,8 @@
 {{--claim--}}
-<div class="form-group ml-3">
+{{-- <div class="form-group ml-3">
     <div class="form-check form-switch">
       <input class="form-check-input" type="checkbox" name="idle" value="{{ old('idle', $eir->idle) }}" {{ $eir->idle == '1' ? 'checked' : '' }}>
     </div>
-</div>
+</div> --}}
+
+{{ $eir->idle == '1' ? 'Yes' : 'No' }}
