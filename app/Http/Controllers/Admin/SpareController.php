6df93d3a9afe7fe76e8c08cd1e4a5e9f5408774d
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-19 10:29:15 +0200

    add

--- app/Http/Controllers/Admin/SpareController.php
+++ app/Http/Controllers/Admin/SpareController.php
@@ -151,7 +151,7 @@ class SpareController extends Controller
     public function update(SpareRequest $request, Spare $spare)
     {
         $validated = $request->validated();
-        dd($validated->usage_date);
+        dd($validated);
         $validated = $request->safe()->except(['attachments','used', 'equipments']);
 
         $validated['used']       = request()->has('used') ? '1' : '0';
