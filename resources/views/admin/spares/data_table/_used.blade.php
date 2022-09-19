d6c78522e3e6be5de18cd9f784561ce14b212c05
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-19 10:18:41 +0200

    add

--- resources/views/admin/spares/data_table/_used.blade.php
+++ resources/views/admin/spares/data_table/_used.blade.php
@@ -1,6 +1,6 @@
 {{--claim--}}
 <div class="form-group ml-3">
     <div class="form-check form-switch">
-      <input class="form-check-input" id="used" type="checkbox" name="used" data-id="{{ $spare->id }}" value="{{ old('used', $spare->used) }}" {{ $spare->used == '1' ? 'checked' : '' }}>
+      <input class="form-check-input" id="used" type="checkbox" name="used" data-id="{{ $spare->id }}" value="{{ old('used', $spare->used) }}" {{ $spare->used == '1' ? 'checked' : '' }} disabled>
     </div>
 </div>
