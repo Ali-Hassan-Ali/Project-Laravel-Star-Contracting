35c672a3d73d3a8b99c718458280b9e96155ff74
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-20 14:22:58 +0200

    add

--- resources/views/admin/maintenances/data_table/_scheduled.blade.php
+++ resources/views/admin/maintenances/data_table/_scheduled.blade.php
@@ -1,6 +1,8 @@
 {{--claim--}}
-<div class="form-group ml-3">
+{{-- <div class="form-group ml-3">
     <div class="form-check form-switch">
       <input class="form-check-input" id="scheduled" type="checkbox" name="scheduled" data-id="{{ $maintenance->id }}" value="{{ old('scheduled', $maintenance->scheduled) }}" {{ $maintenance->scheduled == '1' ? 'checked' : '' }} disabled>
     </div>
-</div>
+</div> --}}
+
+{{ $maintenance->scheduled == '1' ? 'Yes' : 'No' }}
