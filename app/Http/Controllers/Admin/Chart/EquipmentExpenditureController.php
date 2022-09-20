3f2333652ef9e869ea25793224c0336af0f235a7
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-20 10:47:31 +0200

    add

--- app/Http/Controllers/Admin/Chart/EquipmentExpenditureController.php
+++ app/Http/Controllers/Admin/Chart/EquipmentExpenditureController.php
@@ -35,7 +35,7 @@ class EquipmentExpenditureController extends Controller
                          $equipment->driver_salary + 
                          $equipment->spares->sum('cost') + 
                          $equipment->spares->sum('freight_charges') +
-                         $equipment->fuel()->count() > 0 ? $equipment->fuel->sum('total_cost_of_fuel') : 0;
+                         $equipment->fuel()->count() > 0 ? $equipment->fuel()->sum('total_cost_of_fuel') : 0;
                          // $equipment->fuel()->count() > 0 ? 0 : 0;
 
                 $month = $equipment->created_at->format('F');
