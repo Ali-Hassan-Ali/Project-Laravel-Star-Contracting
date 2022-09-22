098a472719839dde5f51689e4e3532aa8a31289f
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-22 15:07:16 +0200

    add

--- resources/views/admin/spares/data_table/_equipment.blade.php
+++ resources/views/admin/spares/data_table/_equipment.blade.php
@@ -6,7 +6,7 @@
 		@endphp
 		
 		@if(isset($equipment))
-			<pre class="badge badge-primary">{{ $equipment->name . ' ' . $equipment->make . ' ' . $equipment->plate_no }}</pre>
+			<pre class="badge badge-primary">{{ $equipment->name . '' . $equipment->make . ' ' . $equipment->plate_no }}</pre>
 		@endif
 
 	@endforeach
