5ea8af46204015725876011ea93ba3e7335861d4
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-26 23:25:31 +0200

    add

--- resources/views/admin/citys/data_table/actions.blade.php
+++ resources/views/admin/citys/data_table/actions.blade.php
@@ -2,7 +2,7 @@
     <a href="{{ route('admin.citys.edit', $id) }}" class="btn btn-warning btn-sm"
         data-html="true" data-placement="right" title="@lang('site.edit')">
         <i class="fa fa-edit"></i> 
-        @lang('site.edit')
+        {{-- @lang('site.edit') --}}
     </a>
 @endif
 
@@ -13,7 +13,7 @@
         <button type="submit" class="btn btn-danger btn-sm delete" 
             data-html="true" data-placement="right" title="@lang('site.delete')">
             <i class="fa fa-trash"></i> 
-            @lang('site.delete')
+            {{-- @lang('site.delete') --}}
         </button>
     </form>
 @endif
