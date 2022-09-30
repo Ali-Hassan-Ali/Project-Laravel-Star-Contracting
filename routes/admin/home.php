74ca183869ded21f102b651782c6a187a8b6961f
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-30 23:33:06 +0200

    add

--- routes/admin/home.php
+++ routes/admin/home.php
@@ -1,24 +1,25 @@
 <?php
-// use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
-// Route::middleware([
-//     // 'localeSessionRedirect',
-//     // 'localizationRedirect',
-//     // 'localeViewPath',
-//     'auth',
-//     'role:admin|super_admin',
-// ])
-//     ->group(function () {
+Route::middleware([
+    // 'localeSessionRedirect',
+    // 'localizationRedirect',
+    // 'localeViewPath',
+    'auth',
+    'role:admin|super_admin',
+])
+    ->group(function () {
 
-//         Route::name('admin.reports.')->prefix('admin/report')->group(function () {
+        Route::name('admin.home.')->prefix('admin/home')->group(function () {
+            // reports Route
+            Route::get('/top_statistics/tables', 'HomeController@topStatisticsTables')->name('top_statistics.tables');
 
-//             // reports Route
+            Route::get('ajax/eir_pending_approved', 'AjaxController@EirPendingApproved')->name('ajax.eir_pending_approved');
+            Route::get('ajax/eir_in_transit', 'AjaxController@EirInTransit')->name('ajax.eir_in_transit');
+            Route::get('ajax/equipment_vehicle', 'AjaxController@EquipmentVehicle')->name('ajax.equipment_vehicle');
+            Route::get('ajax/equipment_rented', 'AjaxController@EquipmentRented')->name('ajax.equipment_rented');
+            Route::get('ajax/equipment_barkdown', 'AjaxController@EquipmentBarkdown')->name('ajax.equipment_barkdown');
 
-//             Route::get('spares_available', 'AparesAvailableController@AparesAvailable')->name('spares_available');
-//             Route::get('spares_available/sum', 'AparesAvailableController@sumAparesAvailable')->name('spares_available.sum');
-//             Route::get('spares_available/data', 'AparesAvailableController@dataAparesAvailable')->name('spares_available.data');
+        });//end of group
 
-
-//         });//end of group
-
-//     });//end of Route middleware
+    });//end of Route middleware
