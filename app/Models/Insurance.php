df2667c9650a7b98dbf312ee65aa9abcc572a27d
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-09-30 21:07:01 +0200

    add

--- app/Models/Insurance.php
+++ app/Models/Insurance.php
@@ -43,11 +43,16 @@ class Insurance extends Model
 
     public function scopeWhereDateBetween($query, $startDate, $endDate)
     {
-        $startDate = now()->createFromFormat('Y-m-d', $startDate);
-        $endDate   = now()->createFromFormat('Y-m-d', $endDate);
+        if ($startDate && $endDate) {
 
-        return $query->whereDate('created_at', '>=',$startDate)
-                     ->whereDate('created_at','<=',$endDate);
+            return $query->whereDate('created_at', '>=',$startDate)
+                         ->whereDate('created_at','<=',$endDate);
+            
+        } else {
+
+            return $query;
+
+        } //end of if
 
     }//end of fun
     
