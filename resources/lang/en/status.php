85b4a852881e74131654e3f31358d131b16e177c
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-10-01 11:42:54 +0200

    add

--- resources/lang/en/status.php
+++ resources/lang/en/status.php
@@ -10,6 +10,6 @@ return [
     'working_status'      => 'Working Status',
     'hours_worked'        => 'Hours Worked',
     'break_down_date'     => 'Breakdown Date',
-    'descrption'          => 'Breakdown Descrption',
+    'descrption'          => 'Breakdown Description',
     'break_down_duration' => 'Breakdown Duration',
 ];
