f2f572430f7473220f0b6800e4ebbbee95aeae50
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-10-01 10:55:43 +0200

    add

--- resources/views/admin/fuels/edit.blade.php
+++ resources/views/admin/fuels/edit.blade.php
@@ -234,7 +234,7 @@
 
     <script>
         
-        $(document).on('change', '#current_mileage_reading, #no-of-unit-filled, #last_mileage_reading',function () {
+        $(document).on('change keyup', '#current_mileage_reading, #no-of-unit-filled, #last_mileage_reading',function () {
 
             var current     = $('#current_mileage_reading').val();
             var lastCurrent = $('#last_mileage_reading').val();
@@ -248,7 +248,7 @@
             
         });//end of change
 
-        $(document).on('change', '#no-of-unit-filled, #fuel_rate_per_litre',function () {
+        $(document).on('change keyup', '#no-of-unit-filled, #fuel_rate_per_litre',function () {
 
             var fuelRate    = $('#fuel_rate_per_litre').val();
             var unit        = $('#no-of-unit-filled').val();
