9d0177e063c4aa90bc26590b717a47b87aae5485
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-10-02 11:35:08 +0200

    add

--- app/Http/Controllers/Admin/Report/FuelConsumptionController.php
+++ app/Http/Controllers/Admin/Report/FuelConsumptionController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin\Report;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\City;
-use App\Models\Equipment;
+use App\Models\Fuel;
 use Yajra\DataTables\DataTables;
 
 class FuelConsumptionController extends Controller
@@ -20,36 +20,35 @@ class FuelConsumptionController extends Controller
 
     public function data()
     {
-
-    	$equipments = Equipment::with('fuel')
+    	$fuels = Fuel::with('equipment')
                                 ->whereDateBetween(request()->start_data, request()->end_data)
                                 ->WhenCityId(request()->city_id)
-                                ->orderBy('city_id')
+                                ->orderBy('id')
                                 ->get();
 
 
-        return DataTables::of($equipments)
-            ->addColumn('city', function (Equipment $equipment) {
-                return $equipment->city->name;
+        return DataTables::of($fuels)
+            ->addColumn('city', function (Fuel $fuel) {
+                return $fuel->equipment->city->name;
             })
-            ->addColumn('name', function (Equipment $equipment) {
-                return $equipment->name . ' ' . $equipment->make . ' ' . $equipment->plate_no;
+            ->addColumn('name', function (Fuel $fuel) {
+                return $fuel->equipment->make . ' ' . $fuel->equipment->name . ' ' . $fuel->equipment->plate_no;
             })
-            ->addColumn('project', function (Equipment $equipment) {
-                return $equipment->allocated_to;
+            ->addColumn('project', function (Fuel $fuel) {
+                return $fuel->project ?? '';
             })
-            ->addColumn('fuel', function (Equipment $equipment) {
-                return $equipment->fuel->unit ?? '';
+            ->addColumn('fuel', function (Fuel $fuel) {
+                return $fuel->unit ?? '';
             })
-            ->addColumn('no_of_units_filled', function (Equipment $equipment) {
-                return $equipment->fuel->no_of_units_filled ?? 0;
+            ->addColumn('no_of_units_filled', function (Fuel $fuel) {
+                return $fuel->no_of_units_filled ?? 0;
             })
             ->addColumn('fuel_rate_per_litre', function (Equipment $equipment) {
-                $data = $equipment->fuel->fuel_rate_per_litre ?? '0';
+                $data = $fuel->fuel_rate_per_litre ?? '0';
                 return "$ $data";
             })
             ->addColumn('total_cost_of_fuel', function (Equipment $equipment) {
-                $data = $equipment->fuel->total_cost_of_fuel ?? '0';
+                $data = $fuel->total_cost_of_fuel ?? '0';
                 return "$ $data";
             })
             ->toJson();
