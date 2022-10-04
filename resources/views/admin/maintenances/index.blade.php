34d2c6ca62579cb3366361afaaac856715c2fa53
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-10-04 17:05:13 +0200

    add

--- resources/views/admin/maintenances/index.blade.php
+++ resources/views/admin/maintenances/index.blade.php
@@ -151,7 +151,7 @@
 
                 console.log(new Date(data.next_service_date));
 
-                if (data.actual_service_date > data.next_service_date) {
+                if (new Date(data.actual_service_date) > new Date(data.next_service_date)) {
 
                     $(row).addClass('bg-danger-datatable text-black');
             
