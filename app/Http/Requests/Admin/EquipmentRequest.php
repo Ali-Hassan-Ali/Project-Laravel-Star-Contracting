7417d65d521be524441a502e037d0ac323fe5e81
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-10-04 10:27:41 +0200

    add

--- app/Http/Requests/Admin/EquipmentRequest.php
+++ app/Http/Requests/Admin/EquipmentRequest.php
@@ -23,8 +23,6 @@ class EquipmentRequest extends FormRequest
      */
     public function rules()
     {
-        dd(request()->all());
-
         $rules = [
             'name'                  => ['required','string','min:1','max:255'],
             'make'                  => ['required','string','min:1','max:255'],
