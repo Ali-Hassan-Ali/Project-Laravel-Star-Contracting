81c6e74f2198b5c4617739ed3de84642785976b7
Ali-Hassan-Ali <alihassanalimadny@gmail.com>
2022-10-04 13:07:33 +0200

    add

--- resources/views/admin/status/create.blade.php
+++ resources/views/admin/status/create.blade.php
@@ -107,8 +107,9 @@
                         {{--break_down_date--}}
                         <div class="form-group col-6">
                             <label>@lang('status.break_down_date') <span class="text-danger">*</span></label>
-                            <input type="date" id="break_down_date" name="break_down_date" disabled
-                                   {{ old('working_status') == 'Breakdown' ? '' : 'disabled' }} class="form-control @error('break_down_date') is-invalid @enderror" value="{{ old('break_down_date') }}" autofocus max="{{ date('Y-m-d', strtotime( now() )) }}">
+                            <input type="date" id="break_down_date" name="break_down_date"
+                                   {{ old('working_status') == 'Breakdown' ? '' : 'disabled' }}
+                                   {{ old('working_status') ? '' : 'disabled' }} class="form-control @error('break_down_date') is-invalid @enderror" value="{{ old('break_down_date') }}" autofocus max="{{ date('Y-m-d', strtotime( now() )) }}">
                             @error('break_down_date')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -134,7 +135,8 @@
                     {{-- break_down_description --}}
                     <div class="form-group">
                         <label>@lang('status.descrption') <span class="text-danger">*</span></label>
-                        <textarea disabled {{ old('working_status') == 'Breakdown' ? '' : 'disabled' }} id="break_down_description" 
+                        <textarea disabled {{ old('working_status') == 'Breakdown' ? '' : 'disabled' }}
+                                           {{ old('working_status') ? '' : 'disabled' }} id="break_down_description" 
                                            class="form-control @error('break_down_description') is-invalid @enderror" name="break_down_description" rows="3">{{ old('break_down_description') }}</textarea>
                         @error('break_down_description')
                             <span class="invalid-feedback" role="alert">
